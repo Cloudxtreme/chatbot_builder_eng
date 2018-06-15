0d6c524f2d81c792304e4d2be7c5de38e411cc43
Administrator <Administrator@172.27.0.96>
2018-06-15 17:19:04 +0900

    commit

--- static/js/chat/chat_mobile.js
+++ static/js/chat/chat_mobile.js
@@ -3,7 +3,6 @@ var $messages = $('.messages-content'),
     i = 0;
 
 $(window).load(function() {
-	$messages.mCustomScrollbar();
 	var notice_list = eval($('#notice_list').val());
 	for (var i = 0; i < notice_list.length; ++i) {
 		var notice = notice_list[i].replace('\n', '<br>');
@@ -11,19 +10,6 @@ $(window).load(function() {
 	}
 	getNewQuestion();
 });
-
-function updateScrollbar() {
-	$('#scroll-down').mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
-	    scrollInertia: 1,
-	    timeout: 0,
-	    contentTouchScroll: true
-	});
-	$('#scroll-top').mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'top', {
-	    scrollInertia: 1,
-	    timeout: 0,
-	    contentTouchScroll: true
-	});
-}
 
 function setDate(){
   d = new Date()
@@ -33,18 +19,21 @@ function setDate(){
   }
 }
 
+function update_scroll() {
+	$('.messages-content').scrollTop($(document).height());
+}
+
 function insertMessage() {
   msg = $('.message-input').val();
   if ($.trim(msg) == '') {
     return false;
   }
-  $('<div class="message message-personal">' + msg + '</div>').appendTo($('#left-board .mCSB_container')).addClass('new');
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.messages-content')).addClass('new');
   setDate();
   $('.message-input').val(null);
-  updateScrollbar();
   interact(msg);
+  update_scroll();
   setTimeout(function() {
-    //fakeMessage();
   }, 1000 + (Math.random() * 20) * 100);
 }
 
@@ -53,7 +42,7 @@ $('.message-submit').click(function() {
 });
 
 function reply_answer(text) {
-	$('<div class="message new"><figure class="avatar"><img src="/static/res/ai_image2.png" /></figure>' + text + '</div>').appendTo($('#left-board .mCSB_container')).addClass('new');
+	$('<div class="message new"><figure class="avatar"><img src="/static/res/ai_image2.png" /></figure>' + text + '</div>').appendTo($('.messages-content')).addClass('new');
 }
 
 $(window).on('keydown', function(e) {
@@ -62,28 +51,6 @@ $(window).on('keydown', function(e) {
 		return false;
 	}
 });
-
-function getNewQuestion() {
-	$.post('/latest_new_question', {
-		user : $('#user').val(),
-		project : $('#project').val()
-	}).done(function(reply) {
-		if (Number(reply['num']) > 0) {
-			$('#message3 .mCSB_container').empty();			
-		}
-		var text = '- newly registered questions in a month -';
-		$('<div class="new-question-msg">' + text + '</div>').appendTo($('#right-down-board .mCSB_container')).addClass('new');
-		for (var i = 0; i < Number(reply['num']); ++i) {
-			var text = reply['text' + (i + 1)];
-				text = text.replace('\n', '');
-			var html = '<div id="latest_new_question" class="latest-new-question">';
-				html += '<img class="check-mark" src="static/res/green_check_mark.png" />&nbsp;&nbsp;<a onclick="setMessageInput(\'' + text.replace(/\"/g, '') + '\')">' + text + '</a></div>';
-			$(html).appendTo($('#right-down-board .mCSB_container')).addClass('new');
-		}
-		updateScrollbar();
-		}).fail(function() {
-				});
-}
 
 function setMessageInput(text) {
 	$('.message-input').val(text);
@@ -171,7 +138,6 @@ function open_popup(url) {
 	var x = (screen.width - w) / 2;
 	var y = (screen.height - h) / 3;
 	window.open(url, '');
-	//, 'scrollbars=yes, resizable=yes, width=' + w + ', height=' + h + ', left=' + x + ', top=' + y
 }
 
 function go_back() {
@@ -179,7 +145,7 @@ function go_back() {
 }
 
 function interact(message){
-	$('<div class="message loading new"><figure class="avatar"><img src="/static/res/ai_image2.png" /></figure><span></span></div>').appendTo($('#left-board .mCSB_container'));
+	$('<div class="message loading new"><figure class="avatar"><img src="/static/res/ai_image2.png" /></figure><span></span></div>').appendTo($('.messages-content'));
 	$.post('/message', {
 		user : $('#user').val(),
 		project : $('#project').val(),
@@ -228,11 +194,11 @@ function interact(message){
 	    image_path_list = eval(reply['image_path']);
 	    if (image_path_list != null && image_path_list != '') {
 		    for (var i = 0; i < image_path_list.length; ++i) {
-		    	$('<div class="message new"><img class="answer-image" src="' + image_path_list[i] + '" /><a href="' + image_path_list[i] + '" target="_blank");">이미지확대</a></div>').appendTo($('#left-board .mCSB_container')).addClass('new');
+		    	$('<div class="message new"><img class="answer-image" src="' + image_path_list[i] + '" /><a href="' + image_path_list[i] + '" target="_blank");">이미지확대</a></div>').appendTo($('.messages-content')).addClass('new');
 		    }
 	    }
 	    if (reply['right_yn'] != '') {
-	    	$('<div id=' + message_count + ' class="message new"><figure class="avatar"><img src="/static/res/ai_image2.png" /></figure>' + reply['right_yn'] + '</div>').appendTo($('#left-board .mCSB_container')).addClass('new');
+	    	$('<div id=' + message_count + ' class="message new"><figure class="avatar"><img src="/static/res/ai_image2.png" /></figure>' + reply['right_yn'] + '</div>').appendTo($('.messages-content')).addClass('new');
 	    }
 	    if (reply['right_yn'] != '' && Number(reply['num']) == 1) {
 	    	question.push(reply['qst']);
@@ -243,7 +209,7 @@ function interact(message){
 	    	setMySchedule();
 	    }
 	    setDate();
-	    updateScrollbar();
+	    update_scroll();
 	}).fail(function() {
 		$('.message.loading').remove();
 		reply_answer("Not trained question. please tell me other question.");
@@ -254,39 +220,9 @@ function call_img() {
 	window.open("./img")
 }
 
-function call_excel(output_file) {
-	file = output_file.substring(1)
-	window.open(file, null);
-}
-
 function lpad(s, len, padding) {
 	while (s.length < len) {
 		s = padding + s;
 	}
 	return s;
-}
-
-function change_background() {
-	var bg = $(".bg").css("background-image");
-	if (bg.indexOf("background1.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background1.jpg", "background2.jpg"));
-	} else if (bg.indexOf("background2.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background2.jpg", "background3.jpg"));
-	} else if (bg.indexOf("background3.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background3.jpg", "background4.jpg"));
-	} else if (bg.indexOf("background4.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background4.jpg", "background5.jpg"));
-	} else if (bg.indexOf("background5.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background5.jpg", "background6.jpg"));
-	} else if (bg.indexOf("background6.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background6.jpg", "background7.jpg"));
-	} else if (bg.indexOf("background7.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background7.jpg", "background8.jpg"));
-	} else if (bg.indexOf("background8.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background8.jpg", "background9.jpg"));
-	} else if (bg.indexOf("background9.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background9.jpg", "background10.jpg"));
-	} else if (bg.indexOf("background10.jpg") != -1) {
-		$(".bg").css("background-image", bg.replace("background10.jpg", "background1.jpg"));
-	}
 }
